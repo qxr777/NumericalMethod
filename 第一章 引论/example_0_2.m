8c243af9c37555a54876c8e37449f13f7a7b51db
qxr777 <qixin_y20005@163.com>
2020-09-04 15:29:47 +0800

    Initial commit

---
+++ 第一章 引论/example_0_2.m
@@ -0,0 +1,22 @@
+% 二分法求方程近似根
+clear;
+f = @(x) x ^ 3 - x -1;
+a = 1;
+b = 1.5;
+tol = 1e-15;
+n = 1;
+
+y0 = f(a);
+while (abs(b - a) > tol)
+    x = (a + b) / 2;
+    y = f(x);
+    if (y * y0 > 0)
+        a = x;
+    else
+        b = x;
+    end
+    n = n + 1;
+end
+x;
+fprintf(' x = %.10d\n', x);
+fprintf('迭代次数: %d\n', n);
